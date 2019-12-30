75101aec81f9042d86790779e0de4d74c86595d5
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-12-30 13:08:19 +0200

    fix version

--- .github/workflows/dumb/portfile.cmake
+++ .github/workflows/dumb/portfile.cmake
@@ -3,7 +3,7 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kode54/dumb
-    REF v2.0.3
+    REF 2.0.3
     SHA512 18b10a507d69a754cdf97fbeae41c17f211a6ba1f166a822276bdb6769d3edc326919067a3f4d1247d6715d7a5a8276669d83b9427e7336c6d111593fb7e36cf
     HEAD_REF master
 )
